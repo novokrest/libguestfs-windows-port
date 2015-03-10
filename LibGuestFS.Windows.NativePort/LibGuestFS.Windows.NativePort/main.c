7a25b664b135358dc009754552df27fe0fb4aa6f
novokrest <novokrest013@gmail.com>
2015-03-10 19:11:44 +0300

    create empty test_guestfs()

--- LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/main.c
+++ LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/main.c
@@ -3,11 +3,13 @@
 
 void test_pcre(void);
 void test_xdr(void);
+void test_guestfs(void);
 
 int main(int args, char* argv[])
 {
     test_pcre();
     test_xdr();
+    test_guestfs();
 
     return 0;
 }
