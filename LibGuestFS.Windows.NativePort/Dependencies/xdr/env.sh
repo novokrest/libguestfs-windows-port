204d0f24c808327863c809e04706ead9cf403bbd
novokrest <novokrest013@gmail.com>
2015-03-10 18:25:19 +0300

    download and compile XDR for Windows

---
+++ LibGuestFS.Windows.NativePort/Dependencies/xdr/env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/bin/:/cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/bin/x86:/usr/include/tirpc/:/usr/include/:/usr/local/include/:/usr/local/lib:/usr/lib:/usr/sbin:${PATH}
+export INCLUDE="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\INCLUDE;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\ATLMFC\INCLUDE;C:\Program Files (x86)\Windows Kits\8.1\include\shared;C:\Program Files (x86)\Windows Kits\8.1\include\um;C:\Program Files (x86)\Windows Kits\8.1\include\winrt;"
+export LIB="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\LIB;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\ATLMFC\LIB;C:\Program Files (x86)\Windows Kits\8.1\lib\winv6.3\um\x86;"
+export LIBPATH="C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\LIB;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\ATLMFC\LIB;C:\Program Files (x86)\Windows Kits\8.1\References\CommonConfiguration\Neutral;C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1\ExtensionSDKs\Microsoft.VCLibs\12.0\References\CommonConfiguration\neutral;"
+export VCINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC"
+export VS110COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools"
+export VS120COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools"
+export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
+#export INCLUDE=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/INCLUDE/:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/ATLMFC/INCLUDE/:/cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/include/shared:/cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/include/um:/cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/include/winrt/
+#export LIB=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/LIB:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/ATLMFC/LIB:/cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/lib/winv6.3/um/x86
+#export LIBPATH=/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/LIB:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/LIB/ATLMFC/LIB:/cygdrive/c/Program\ Files\ \(x86\)/Windows\ Kits/8.1/References/CommonConfiguration/Neutral:/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ SDKs/Windows/v8.1/ExtensionSDKs/Microsoft.VCLibs/12.0/References/CommonConfiguration/neutral
+#export VCINSTALLDIR=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/VC/VisualStudioVersion=12.0
+#export VS110COMNTOOLS=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 11.0/Common7/Tools/
+#export VS120COMNTOOLS=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/Common7/Tools/
+#export VSINSTALLDIR=/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 12.0/
