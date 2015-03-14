456dc3e10c542294fcd02a7abc5d332288fea165
novokrest <novokrest013@gmail.com>
2015-03-14 10:23:16 +0300

    check for using libxml

--- LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/main.c
+++ LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/main.c
@@ -4,11 +4,14 @@
 void test_pcre(void);
 void test_xdr(void);
 void test_guestfs(void);
+void test_libxml(void);
 
 int main(int args, char* argv[])
 {
     test_pcre();
     test_xdr();
+    test_libxml();
+
     test_guestfs();
 
     return 0;
---
+++ LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/test-libxml.c
@@ -0,0 +1,9 @@
+#include <libxml/parser.h>
+#include <libxml/xmlversion.h>
+
+void
+test_libxml()
+{
+    xmlInitParser();
+    LIBXML_TEST_VERSION;
+}
