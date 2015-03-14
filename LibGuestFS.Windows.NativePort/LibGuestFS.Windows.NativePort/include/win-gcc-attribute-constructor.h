bb9170edcee1652347f700a3eb8617fc13640bef
novokrest <novokrest013@gmail.com>
2015-03-14 12:00:51 +0300

    win-port: unistd.h and __attribute__(constructor)

---
+++ LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/include/win-gcc-attribute-constructor.h
@@ -0,0 +1,43 @@
+/* 
+ * http://stackoverflow.com/questions/1113409/attribute-constructor-equivalent-in-vc
+ * http://stackoverflow.com/questions/22763945/dll-main-on-windows-vs-attribute-constructor-entry-points-on-linux
+ * http://rsdn.ru/forum/cpp/2979447.all
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef _MSC_VER
+
+#define CCALL __cdecl
+#pragma section(".CRT$XCU",read)
+#define INITIALIZER(f) \
+   static void __cdecl f(void); \
+   __declspec(allocate(".CRT$XCU")) void (__cdecl*f##_)(void) = f; \
+   static void __cdecl f(void)
+
+#elif defined(__GNUC__)
+
+#define CCALL
+#define INITIALIZER(f) \
+   static void f(void) __attribute__((constructor)); \
+   static void f(void)
+
+#endif
+
+static void CCALL finalize(void)
+{
+    printf("finalize\n");
+}
+
+INITIALIZER(initialize)
+{
+    printf("initialize\n");
+    atexit(finalize);
+}
+
+int CCALL main(int argc, const char*const* argv)
+{
+    printf("main\n");
+    return 0;
+}
