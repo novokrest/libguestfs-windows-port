11e2d2350a264d090246da4ffb1ae27c75031f3d
novokrest <novokrest013@gmail.com>
2015-03-17 11:58:04 +0300

    change path to disk.img in qemu running cmd

--- LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/src/launch-direct.c
+++ LibGuestFS.Windows.NativePort/LibGuestFS.Windows.NativePort/src/launch-direct.c
@@ -715,13 +715,14 @@ launch_direct (guestfs_h *g, void *datav, const char *arg)
   //    " -append \"panic=1 console=ttyS0 udevtimeout=6000 udev.event-timeout=6000 no_timer_check acpi=off printk.time=1 cgroup_disable=memory root=/dev/vdb selinux=0 guestfs_verbose=1 TERM=xterm\"";
 
   cmd = "C:/cygwin64/usr/qemu/qemu-system-x86_64.exe" \
+      " -display none" \
       " -m 500" \
       " -no-reboot" \
       " -rtc driftfix=slew" \
       " -no-kvm-pit-reinjection" \
       " -kernel C:/GuestFS/appliance/kernel " \
       " -initrd C:/GuestFS/appliance/initrd " \
-      " -drive file=C:/GuestFS/disk.img,cache=writeback,format=raw,id=hd0,if=none" \
+      " -drive file=C:/GuestFS/appliance/disk.img,cache=writeback,format=raw,id=hd0,if=none" \
       " -device virtio-blk-pci,drive=hd0" \
       " -drive file=C:/GuestFS/appliance/root,snapshot=on,id=appliance,cache=unsafe,if=none" \
       " -device virtio-blk-pci,drive=appliance" \
